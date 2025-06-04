ccb811b3b0fcab14b54291b9e28640f3eb589b0f
Jeff <jefferson.vivas.95@gmail.com>
2025-06-05 01:26:36 +0200

    update: diseño e implementacion de campos en el modal para crear movimientos

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -4,8 +4,8 @@ const Modal = ({ isOpen, onClose, children}) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div className="bg-white rounded-2xl shadow-lg p-6 max-w-md w-full relative">
+        <div className="fixed inset-0 flex items-center justify-center text-black z-50">
+            <div className="bg-blue-400 bg-opacity-50 rounded-2xl shadow-lg p-6 max-w-md w-full relative">
                 <button
                     className="absolute top-3 right-4 text-gray-500 hover:text-red-500 text-xl"
                     onClick={onClose}
--- frontend/src/layouts/SidebarLayout.jsx
+++ frontend/src/layouts/SidebarLayout.jsx
@@ -17,7 +17,27 @@ const Layout = ({ children }) => {
                 isOpen={isModalOpen}
                 onClose={() => setIsModalOpen(false)}
             >
-                <h2>Nuevo movimiento</h2>
+                <h2 className='text-center text-2xl'>NUEVO MOVIMIENTO</h2>
+                <form className="flex flex-col gap-4 mt-4 space-y-3">
+                    <div>
+                        <label>Concepto</label>
+                        <input
+                            type='text'
+                            className='w-full h-fit p-2 border border-gray-300 rounded bg-blue-300 text-black'
+                        />
+                    </div>
+                                        <div>
+                        <label>Importe</label>
+                        <input
+                            type='text'
+                            className='w-full h-fit p-2 border border-gray-300 rounded bg-blue-300 text-black'
+                        />
+                    </div>
+                    <div className='justify-between flex'>
+                        <button className='bg-green-300 text-black hover:bg-emerald-400 transition duration-500 p-2 border rounded'>Añadir</button>
+                        <button className='bg-red-300 text-black hover:bg-rose-400 transition duration-500 p-2 border rounded'>Cancelar</button>
+                    </div>
+                </form>
             </Modal>
         </div>
     );
