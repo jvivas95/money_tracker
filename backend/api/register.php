ed73a0e68f17f284550b3bafcac99015993a6bc2
Jeff <jefferson.vivas.95@gmail.com>
2025-06-11 14:49:16 +0200

    update: finalización de stmt para registrar un usuario

--- backend/api/register.php
+++ backend/api/register.php
@@ -8,4 +8,29 @@ header("Access-Control-Allow-Methods: POST");
 require_once __DIR__ . './../config/db.php';
 
 // Get data from body
-$data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents("php://input", true), true);
+
+if (!isset($data['username']) || !isset($data['password']) || !isset($data['email'])) {
+    http_response_code(400);
+    echo json_encode(["message" => "Invalid input"]);
+    exit();
+}
+
+// Dates on variables
+$username = htmlspecialchars(strip_tags(($data->username)));
+$password = password_hash($data->password, PASSWORD_DEFAULT);
+$email = htmlspecialchars(strip_tags(($data->email)));
+
+// Prepare SQL statement
+$stmt = $conn->prepare("INSERT INTO usuarios (username, password, email) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $username, $password, $email);
+
+// Frontend response
+if($stmt->execute()){
+    http_response_code(201);
+    echo json_encode(["message" => "Usuario registrado correctamente"]);
+}
+else{
+    http_response_code(500);
+    echo json_encode(["message" => "Error al registrar al usuario"]);
+}
